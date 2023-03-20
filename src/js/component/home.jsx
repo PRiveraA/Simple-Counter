160692269fd74db124ddcd013a937defbf29ed03
PRiveraA <priveraa94@gmail.com>
2023-03-20 03:31:03 +0000

    Completed counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,4 @@
 import React from "react";
 
-//create your first component
-const Home = () => {
-	return (
-		<h1>Hello</h1>
-	);
-};
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,50 @@ import ReactDOM from "react-dom";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+function SimpleCounter({
+  onesPlace,
+  tensPlace,
+  hundredsPlace,
+  thousandsPlace,
+  tenThousandsPlace,
+  hundredThousandsPlace,
+}) {
+  return (
+    <div className="container">
+      <div className="">
+        <i className="far fa-clock"></i>
+      </div>
+      <div className="s hundredThousands">{hundredThousandsPlace}</div>
+      <div className="s tenThousands">{tenThousandsPlace}</div>
+      <div className="s thousands">{thousandsPlace}</div>
+      <div className="s hundreds">{hundredsPlace}</div>
+      <div className="s tens">{tensPlace}</div>
+      <div className="s ones">{onesPlace}</div>
+    </div>
+  );
+}
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(function () {
+  const hundredThousands = Math.floor(counter / 100000 % 10);
+  const tenThousands = Math.floor(counter / 10000 % 10);
+  const thousands = Math.floor(counter / 1000 % 10);
+  const hundreds = Math.floor(counter / 100 % 10);
+  const tens = Math.floor(counter / 10 % 10);
+  const ones = Math.floor(counter / 1 % 10);
+  console.log(ones);
+  counter++;
+
+  ReactDOM.render(
+    <SimpleCounter
+      onesPlace={ones}
+      tensPlace={tens}
+      hundredsPlace={hundreds}
+      thousandsPlace={thousands}
+      tenThousandsPlace={tenThousands}
+      hundredThousandsPlace={hundredThousands}
+       />,
+    document.querySelector("#app")
+  );
+}, 1000);
+
